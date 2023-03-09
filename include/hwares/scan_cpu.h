427b0c295ce2cf60b877ff2622891a864e4c3835
ahsouza <annibalhsouza@gmail.com>
2023-03-09 15:10:30 -0300

    feat: new headers to hardwares on computer

---
+++ include/hwares/scan_cpu.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace bscan {
+
+  struct InstructionSet {
+    bool _isHTT = false;
+    bool _isSSE = false;
+    bool _isSSE2 = false;
+    bool _isSSE3 = false;
+    bool _isSSE41 = false;
+    bool _isSSE42 = false;
+    bool _isAVX = false;
+    bool _isAVX2 = false;
+
+    bool _init_ = false;
+  };
+
+  class CPU {
+    friend std::optional<CPU> getCPU(uint8_t socket_id);
+
+   public:
+    CPU() = default;
+    CPU(int id);
+    ~CPU() = default;
+
+    std::string& modelName();
+    std::string& vendor();
+    int cacheSize_Bytes();
+    int numPhysicalCores();
+    int numLogicalCores();
+    int maxClockSpeed_kHz();
+    int regularClockSpeed_kHz();
+    InstructionSet& instructionSet();
+
+    static int currentClockSpeed_kHz();
+
+    static std::string getModelName();
+    static std::string getVendor();
+    static int getNumPhysicalCores();
+    static int getNumLogicalCores();
+    static int getMaxClockSpeed_kHz();
+    static int getRegularClockSpeed_kHz();
+    static int getCacheSize_Bytes();
+
+   private:
+    std::string _modelName;
+    std::string _vendor;
+    int _numPhysicalCores = -1;
+    int _numLogicalCores = -1;
+    int _maxClockSpeed_kHz = -1;
+    int _regularClockSpeed_kHz = -1;
+    int _cacheSize_Bytes = -1;
+    InstructionSet _instructionSet;
+
+    int _id = 0;
+  };
+
+  class Socket {
+   public:
+    explicit Socket(uint8_t id);
+    Socket(uint8_t id, const CPU& cpu);
+    ~Socket() = default;
+
+    class CPU& CPU();
+
+   private:
+    uint8_t _id;
+    class CPU _cpu;
+  };
+
+  std::optional<CPU> getCPU(uint8_t socket_id);
+
+  std::vector<Socket> getAllSockets();
+
+};
---
+++ include/hwares/scan_cpuid.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "platform.h"
+
+#if defined(BSCAN_X86)
+
+#ifdef _MSC_VER
+#include <Windows.h>
+#else
+#include <cpuid.h>
+#endif
+
+#define MAX_INTEL_TOP_LVL 4
+
+#define SSE_POS 0x02000000
+#define SSE2_POS 0x04000000
+#define SSE3_POS 0x00000001
+#define SSE41_POS 0x00080000
+#define SSE42_POS 0x00100000
+#define AVX_POS 0x10000000
+#define AVX2_POS 0x00000020
+#define LVL_NUM 0x000000ff
+#define LVL_TYPE 0x0000ff00
+#define LVL_CORES 0x0000ffff
+
+namespace bscan::cpuid {
+
+/**
+ * cpuid wrapper for Linux, MacOS and Windows
+ * @param func_id
+ * @param sub_func_id
+ * @param regs
+ */
+inline void cpuid(uint32_t func_id, uint32_t sub_func_id, uint32_t regs[4]) {
+#ifdef _MSC_VER
+  __cpuidex(reinterpret_cast<int*>(regs), static_cast<int>(func_id), static_cast<int>(sub_func_id));
+#elif defined(__GNUC__) || defined(__clang__)
+  __get_cpuid_count(func_id, sub_func_id, &regs[0], &regs[1], &regs[2], &regs[3]);
+#elif __CYGWIN
+  cpuid(&regs[0], &regs[1], &regs[2], &regs[3], func_id, sub_func_id);
+#endif
+}
+
+}  // example namespace:  bscan::cpuid
+
+#endif
