e6c96732c46f86a2134484c7ec72721ba4f31a28
ahsouza <annibalhsouza@gmail.com>
2023-03-12 00:12:31 -0300

    add new resources to get informations

--- src/libs/info/bscan.cpp
+++ src/libs/info/bscan.cpp
@@ -1,13 +1,31 @@
 #include <napi.h>
 #include <string>
-#include "../../../include/hwares/hwares.h"
-#include "../../../include/swares/swares.h"
+#include "hwares/hwares.h"
+#include "swares/swares.h"
 
 Napi::String cpu(const Napi::CallbackInfo& info) {
 	Napi::Env env = info.Env();
   bscan::CPU cpu;
 
-	std::string result = cpu.vendor();
+	std::string result = "Fabricante: %s\nModelo: %s\n", cpu.vendor(), cpu.getModelName();
+
+	return Napi::String::New(env, result);
+}
+
+Napi::String ram(const Napi::CallbackInfo& info) {
+	Napi::Env env = info.Env();
+  bscan::RAM ram;
+
+	std::string result = ram.getMemory_Bytes();
+
+	return Napi::String::New(env, result);
+}
+
+Napi::String mainboard(const Napi::CallbackInfo& info) {
+	Napi::Env env = info.Env();
+  bscan::MainBoard mo;
+
+	std::string result = mo.getModel();
 
 	return Napi::String::New(env, result);
 }
@@ -18,6 +36,10 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
 	exports.Set(
 		Napi::String::New(env, "cpu"),
 		Napi::Function::New(env, cpu)
+    Napi::String::New(env, "mainboard"),
+		Napi::Function::New(env, mainboard)
+    Napi::String::New(env, "ram"),
+		Napi::Function::New(env, ram)
 	);
 
 	return exports;
