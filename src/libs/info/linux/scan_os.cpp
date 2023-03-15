c6d96a30d7c77a131c1f6fa97df9a82acb0a51e0
ahsouza <annibalhsouza@gmail.com>
2023-03-15 17:04:03 -0300

    copyright

--- src/libs/info/linux/scan_os.cpp
+++ src/libs/info/linux/scan_os.cpp
@@ -1,3 +1,20 @@
+/************************************************************************************
+      
+ *      Copyright (C) 2021 - 2023, Barca Web Cloud, Inc. 
+ *    Email: <opensource@barca.com>  GitHub: @BarcaWebCloud. 
+ 
+ * This software is licensed as described in the file COPYING, which                    
+ * you should have received as part of this distribution. The terms                     
+ * are also available at https://project-barca.github.io/docs/copyright.html.           
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell             
+ * copies of the Software, and permit persons to whom the Software is                   
+ * furnished to do so, under the terms of the COPYING file.                             
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY            
+ * KIND, either express or implied.                                                      
+ *
+ **************************************************************************************/
 #include "platform.h"
 
 #ifdef BSCAN_UNIX
@@ -72,7 +89,6 @@ namespace bscan {
     return std::string(line);
   }
 
-
   std::string OS::getUsersWithHome() {
     std::string command("getent passwd | grep '/home' | cut -d: -f1 | awk 'FNR>=2'");
     std::string output = exec(command);
@@ -114,6 +130,18 @@ namespace bscan {
     std::ifstream stream("/proc/sys/kernel/domainname");
     if (!stream) {
       return "Linux <unknown domain name>";
+    }
+    getline(stream, line);
+    stream.close();
+
+    return std::string(line);
+  }
+
+  std::string OS::getLog() {
+    std::string line;
+    std::ifstream stream("/var/log/boot.log");
+    if (!stream) {
+      return "Linux <unknown hostname>";
     }
     getline(stream, line);
     stream.close();
