5f393bea1374843184c63213f42aad572f691ebf
ahsouza <annibalhsouza@gmail.com>
2023-04-01 22:41:07 -0300

    scan browser

---
+++ src/libs/info/scan_browser.cpp
@@ -0,0 +1,34 @@
+/************************************************************************************
+      
+ *                 Copyright (C) 2021 - 2023, Barca, Inc. 
+ 
+ *    Email: <opensource@barca.com>  GitHub: @BarcaWebCloud. 
+ *    Project: BSCAN to scanner MotherBoards. CPU, Memory Ram, SO and more
+ 
+ * This software is licensed as described in the file COPYING, which                    
+ * you should have received as part of this distribution. The terms                     
+ * are also available at https://project-barca.github.io/docs/copyright.html.           
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell             
+ * copies of the Software, and permit persons to whom the Software is                   
+ * furnished to do so, under the terms of the COPYING file.                             
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY            
+ * KIND, either express or implied.                                                      
+ *
+ **************************************************************************************/
+#include "swares/scan_browser.h"
+#include <string>
+
+namespace bscan {
+
+  Browser::Browser(){};
+  
+  std::string Browser::history() {
+    if (_history.empty()) {
+      _history = getHistory();
+    }
+    return _history;
+  }
+
+};
