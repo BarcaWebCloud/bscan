bcacdf7b6cc18c352c5172998f18eea8f961af32
ahsouza <annibalhsouza@gmail.com>
2023-04-02 23:43:53 -0300

    config header

---
+++ include/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H_IN
+#define CONFIG_H_IN
+ 
+#define MODULE_NAME "bscan"
+#define VERSION_MAJOR "1"
+#define VERSION_MINOR "5"
+#define VERSION_PATCH "0.1.5"
+#define MODULE_VERSION "0.1.5"
+ 
+#endif // CONFIG_H_IN
