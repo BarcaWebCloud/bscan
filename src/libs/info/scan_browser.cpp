7478516b1f4f258ce94f15f910a0738558cb7c80
ahsouza <annibalhsouza@gmail.com>
2023-04-03 14:39:55 -0300

    new features to scanner chrome browser

--- src/libs/info/scan_browser.cpp
+++ src/libs/info/scan_browser.cpp
@@ -45,4 +45,53 @@ namespace bscan {
     return _chromemedia;
   }
 
+  std::string Browser::chromeshortcuts() {
+    if (_chromeshortcuts.empty()) {
+      _chromeshortcuts = getChromeShortcuts();
+    }
+    return _chromeshortcuts;
+  }
+
+  std::string Browser::chromememberof() {
+    if (_chromememberof.empty()) {
+      _chromememberof = getChromeMemberOf();
+    }
+    return _chromememberof;
+  }
+
+  std::string Browser::chromeautofillemails() {
+    if (_chromeautofillemails.empty()) {
+      _chromeautofillemails = getChromeAutoFillEmails();
+    }
+    return _chromeautofillemails;
+  }
+  
+  std::string Browser::chromeautofilladdresses() {
+    if (_chromeautofilladdresses.empty()) {
+      _chromeautofilladdresses = getChromeAutoFillAddresses();
+    }
+    return _chromeautofilladdresses;
+  }
+
+  std::string Browser::chromeautofillphones() {
+    if (_chromeautofillphones.empty()) {
+      _chromeautofillphones = getChromeAutoFillPhones();
+    }
+    return _chromeautofillphones;
+  }
+
+  std::string Browser::chromeautofillnames() {
+    if (_chromeautofillnames.empty()) {
+      _chromeautofillnames = getChromeAutoFillNames();
+    }
+    return _chromeautofillnames;
+  }
+
+  std::string Browser::chromeautofillprofiles() {
+    if (_chromeautofillprofiles.empty()) {
+      _chromeautofillprofiles = getChromeAutoFillProfiles();
+    }
+    return _chromeautofillprofiles;
+  }
+
 };
