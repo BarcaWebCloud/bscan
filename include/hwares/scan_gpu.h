de014b25beaddd3781d8e9842eb10226972fe69c
ahsouza <annibalhsouza@gmail.com>
2023-04-12 14:47:44 -0300

    new header methods to gpu

--- include/hwares/scan_gpu.h
+++ include/hwares/scan_gpu.h
@@ -27,24 +27,63 @@ namespace bscan {
   class GPU {
    public:
     GPU() = default;
-    GPU(const std::string& vendor, const std::string& name, const std::string& driverVersion, int64_t memory_Bytes);
+    GPU(const std::string& vendor,
+        const std::string& name,
+        const std::string& processor,
+        const std::string& driverVersion,
+        const std::string& deviceID,
+        const std::string& status,
+        const std::string& installedDriversPath,
+        int64_t architecture,
+        int64_t type,
+        int64_t mode,
+        int64_t availability,
+        int64_t maxMemory,
+        int64_t memoryBytes);
     ~GPU() = default;
 
     std::string& vendor();
     std::string& name();
+    std::string& processor();
     std::string& driverVersion();
-    int64_t memory_Bytes();
+    std::string& deviceID();
+    std::string& status();
+    std::string& installedDriversPath();
+    int64_t architecture();
+    int64_t type();
+    int64_t mode();
+    int64_t availability();
+    int64_t memoryBytes();
+    int64_t maxMemory();
 
     static std::string getVendor();
     static std::string getName();
+    static std::string getProcessor();
     static std::string getDriverVersion();
-    static int64_t getMemory_Bytes();
+    static std::string getDeviceID();
+    static std::string getStatus();
+    static std::string getInstalledDriversPath();
+    static int64_t getType();
+    static int64_t getMode();
+    static int64_t getArchitecture();
+    static int64_t getAvailability();
+    static int64_t getMaxMemory();
+    static int64_t getMemoryBytes();
 
    private:
     std::string _vendor;
     std::string _name;
+    std::string _processor;
     std::string _driverVersion;
-    int64_t _memory_Bytes = -1;
+    std::string _deviceID;
+    std::string _status;
+    std::string _installedDriversPath;
+    int64_t _architecture = -1;
+    int64_t _type = -1;
+    int64_t _mode = -1;
+    int64_t _availability = -1;
+    int64_t _memoryBytes = -1;
+    int64_t _maxMemory = -1;
   };
 
 };
